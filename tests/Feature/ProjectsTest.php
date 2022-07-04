21a5cd333cccb3092978ec075b480052e20641c9
bozeak <bozeak@gmail.com>
2022-07-04 10:57:00 +0300

    Proejcts scoping to an owner.

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -15,10 +15,27 @@ class ProjectsTest extends TestCase
     /**
      * @test
      */
-    public function only_authenticated_user_can_create_project()
+    public function guests_cannot_create_projects()
     {
         $attributes = Project::factory()->raw();
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /**
+     * @test
+     */
+    public function guests_cannot_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /**
+     * @test
+     */
+    public function guests_cannot_view_a_single_project()
+    {
+        $project = Project::factory()->create();
+        $this->get($project->path())->assertRedirect('login');
     }
 
     /**
@@ -45,15 +62,29 @@ class ProjectsTest extends TestCase
     /**
      * @test
      */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
+        $this->be(User::factory()->create());
+
         $this->withoutExceptionHandling();
 
-        $project = Project::factory()->create();
+        $project = Project::factory()->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /**
+     * @test
+     */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(User::factory()->create());
+
+        $project = Project::factory()->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     /**
