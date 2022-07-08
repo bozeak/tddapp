9e0f243c5268157b1463a2ee0ff7b7cf62b81535
bozeak <bozeak@gmail.com>
2022-07-08 13:34:28 +0300

    Tasks UI improvement.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -21,13 +21,13 @@ class Project extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function addTask($body)
-    {
-        return $this->tasks()->create(compact('body'));
-    }
-
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function addTask($body)
+    {
+        return $this->tasks()->create(compact('body'));
     }
 }
