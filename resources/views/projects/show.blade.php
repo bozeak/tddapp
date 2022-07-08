21caa44e453ddd66838acd38722aacccefc603bd
bozeak <bozeak@gmail.com>
2022-07-08 14:24:13 +0300

    Tasks updating UI and improvements.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -17,7 +17,17 @@
 
                     {{-- tasks --}}
                     @foreach($project->tasks as $task)
-                        <div class="card mb-3">{{ $task->body }}</div>
+                        <div class="card mb-3">
+                            <form method="post" action="{{ $task->path() }}">
+                                @method('PATCH')
+                                @csrf
+
+                                <div class="flex">
+                                    <input name="body" type="text" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-gray' : '' }}">
+                                    <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}/>
+                                </div>
+                            </form>
+                        </div>
                     @endforeach
 
                     <div class="card mb-3">
