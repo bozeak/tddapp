b16fe6f558bbef55de4d13e80d27d511c8dd164e
bozeak <bozeak@gmail.com>
2022-07-11 17:39:52 +0300

    Add Activity model and Project observer.

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Activity;
+use App\Models\Project;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        $this->recordActivity($project, 'created');
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        $this->recordActivity($project, 'updated');
+    }
+
+    /**
+     * @param $project
+     * @param $type
+     * @return void
+     */
+    protected function recordActivity($project, $type)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => $type,
+        ]);
+    }
+}
